a8e0b4e26cfedca27bdeb4f59995bc0129fa3385
John_Adewumi2 <j.adewumi@gmail.com>
2021-12-03 17:50:14 -0800

    new

--- crontab -e.sh
+++ crontab -e.sh
@@ -1,2 +1,2 @@
-#*/5 * * * *  /usr/bin/python3 /home/ec2-user/xchange/runscripts.sh    #run every minutes
-#0 * * * *  /usr/bin/python3 /home/ec2-user/xchange/scripts/runscripts.sh        #run every hour
+*/5 * * * *  /usr/bin/python3 /home/ec2-user/xchange/runscripts.sh    #run every minutes
+0 * * * *  /usr/bin/python3 /home/ec2-user/xchange/scripts/runscripts.sh        #run every hour
--- xchange_scrip.py
+++ xchange_scrip.py
@@ -1,41 +1,7 @@
+
 import json
+import boto3
 import requests
-import boto3
-import forex_python
-from datetime import datetime
-now_latest=datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
-
-from forex_python.converter import CurrencyRates
-cu = CurrencyRates ()
-rate = cu.get_rates('USD')
-rate_json= now_latest + '.json'
-#print(rate)
-
-#serialized json
-json_file = json.dumps(rate, indent = 5)
-
-#write to file
-with open(rate_json, 'w') as outfile:
-    outfile.write(json_file)
-
-sns_client = boto3.client('sns')
-s3_client = boto3.client('s3', region_name= 'us-east-1')
-s3_name = now_latest + '.json'
-
-with open (rate_json, 'rb') as f:
-    s3.upload_file(f, 'exchange-rate-bucket1', s3_name)
-#print(rate_json)
-
-sns_client.publish(TargetArn = 'arn:aws:sns:us-east-1:672432851135:ExchangeRate', Message = 
-'Hello Team \n below is the file that is being drop in your s3 bucket for latest exchnage rate' + rate_json)
-#print(rate_json)
-
-
-
-
-
-import json
-import boto3
 import os
 import forex_python
 from dotenv import load_dotenv
@@ -51,14 +17,14 @@ rate_json= now_latest + '.json'
 #print(rate)
 
 #serialized json
-json_file = json.dumps(rate, indent = 5)
+json_file = json.dumps(rate, indent = 4)
 
 #define s3 properties to upload
 def upload_to_s3(rate_json):
     file_name = rate_json
     bucket_name = 'exchange-rate-bucket1'
     s3_client = boto3.client(
-    's3',
+    's3', 
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
 )
@@ -70,31 +36,19 @@ def upload_to_s3(rate_json):
 #write to file
 with open(rate_json, 'w') as outfile:
     outfile.write(json_file)
-    upload_to_s3(json_file)
+    upload_to_s3(rate_json)
 
 
-# AWS_ACCESS_KEY_ID=AKIAZZEA7BC7QHVCAI23
-# AWS_SECRET_ACCESS_KEY=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
+# send SNS message with warning and bucket policy
+sns = boto3.client("sns", region_name='us-west-1')
+bucket_name = 'exchange-rate-bucket1'
+subject =  rate_json + " has been uploaded"
+message = 'Hello, the latest exchange rate has been uploaded in the' + bucket_name + 'kindly update the customer data for the new market rate'
 
 
+response = sns.publish(
 
-
-# import json
-# import requests
-# import boto3
-# import forex_python
-# from datetime import datetime
-# now_latest=datetime.today().strftime('%Y_%m_%d_%H_%M_%S')
-
-# from forex_python.converter import CurrencyRates
-# cu = CurrencyRates ()
-# rate = cu.get_rates('USD')
-# rate_json= now_latest + '.json'
-# #print(rate)
-
-# #serialized json
-# json_file = json.dumps(rate, indent = 5)
-
-# #write to file
-# with open(rate_json, 'w') as outfile:
-#     outfile.write(json_file)
+    TopicArn = 'arn:aws:sns:us-east-1:672432851135:ExchangeRate',
+    Subject = subject,
+    Message = message
+)
